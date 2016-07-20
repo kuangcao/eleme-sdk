a9f37e68682c0806e47bf512542f1eb31017b5a0
huwei <huwei@eking.mobi>
2016-07-21 00:12:50 +0800

    实现评论服务

---
+++ src/main/java/com/jiabangou/eleme/sdk/api/impl/CommentServiceImpl.java
@@ -0,0 +1,7 @@
+package com.jiabangou.eleme.sdk.api.impl;
+
+/**
+ * Created by freeway on 16/7/20.
+ */
+public class CommentServiceImpl {
+}
---
+++ src/main/java/com/jiabangou/eleme/sdk/model/Comment.java
@@ -0,0 +1,7 @@
+package com.jiabangou.eleme.sdk.model;
+
+/**
+ * Created by freeway on 16/7/20.
+ */
+public class Comment {
+}
